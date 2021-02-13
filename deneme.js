2c58ff51f66b880af0fe9d8064d9a1bf5877fb16
tacsizcesper <78989433+tacsizcesper@users.noreply.github.com>
2021-02-13 16:52:03 +0300

    Revert "Deneme.js"

--- deneme.js
+++ deneme.js
@@ -7,4 +7,4 @@ Asena.addCommand({pattern: 'deniyorum', fromMe: true}, (async (message, match) =
 
     await message.sendMessage('Bu bir testtir!');
     await new Promise(r => setTimeout(r, 5));
-    await message.sendMessage('başarılı!');
+    await message.sendMessage('Bu e!');
